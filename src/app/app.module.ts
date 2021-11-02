532655008a9b0df96b8296e52036c177f65a8b8d
Pierre <53476143+RoYaLSleT@users.noreply.github.com>
2021-11-02 17:26:26 +0100

    fin quete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserProfilComponent } from './user-profil/user-profil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-profil/user-profil.component.ts
@@ -0,0 +1,22 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-user-profil',
+  templateUrl: './user-profil.component.html',
+  styleUrls: ['./user-profil.component.css']
+})
+export class UserProfilComponent {
+  user = {
+    name : 'Doe',
+    firstName : 'John',
+    age : 25,
+    quote : '',
+    photo : 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+
+  isAgeShown:boolean = true;
+
+  toggleAge(): void {
+    this.isAgeShown = !this.isAgeShown
+  }
+}
